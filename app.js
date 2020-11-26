cdfe97e2babd98ecfcaecf69364226f586ec45b7
20173163 <wjddn494@soongsil.ac.kr>
2020-11-26 11:44:23 +0900

    bootstrap with no ejs

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
+const http = require("http");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-/* #region EJS */
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/public/views");
-/* #endregion */
-
-/* #region main page */
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/mypage", (req, res) => {
-  res.render("mypage");
-});
-
-app.get("/cardio", (req, res) => {
-  res.render("cardio");
-});
-/* #endregion */
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
-
-/* #region CSS, JS*/
-app.use(express.static(__dirname + "/public"));
-/* #endregion */
