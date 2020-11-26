cd0f41acbe777d38d28f4d32a02624bfc6fb05dd
20173163 <wjddn494@soongsil.ac.kr>
2020-11-26 17:22:18 +0900

    mysql

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const http = require("http");
+const { Router } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+/* #region for Static HTML */
+app.use(express.static("public"));
+/* #endregion */
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
