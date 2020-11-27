83faa35ecff263856c5dd9517d3b57d255a2123a
20173163 <wjddn494@soongsil.ac.kr>
2020-11-27 22:26:54 +0900

    mypage ejs ifelse

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,8 @@ const router = express.Router();
 
 const DocumentAPI = require("./document");
 const AuthAPI = require("./auth");
-const MyinfoAPI = require("./myinfo");
 
 router.use("/doc", DocumentAPI);
 router.use("/auth", AuthAPI);
-router.use("/myinfo", MyinfoAPI);
 
 module.exports = router;
--- routes/view/mypage.js
+++ routes/view/mypage.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../../models");
+
 
 router.get("/", function (req, res, next) {
   res.render("mypage", { title: "HealthGallery" });
+});
+
+router.get("/myinfo", async function (req, res, next) {
+  if (req.user == undefined) {
+    console.log("user undefined");
+    return res.sendStatus(400);
+  }
+
+  let user = req.user.id;
+  console.log("user: ", user);
+
+  try {
+    let info = await db.User.findOne({
+      where: {
+        id: user,
+      },
+    });
+    console.log("info:", info);
+    res.render("mypage.ejs", data=info);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
 });
 
 module.exports = router;
