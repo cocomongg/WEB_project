bc9b959aa59c5adfba5b1f63c81b277ee477b4c1
20173163 <wjddn494@soongsil.ac.kr>
2020-11-29 02:40:38 +0900

    for merge

--- routes/view/index.js
+++ routes/view/index.js
@@ -3,10 +3,9 @@ const router = express.Router();
 
 const mainView = require("./main");
 const mypageView = require("./mypage");
-const postView = require("./post");
+
 // TODO: 다른 페이지 추가
 router.use("/", mainView);
 router.use("/mypage", mypageView);
-router.use("/post", postView);
 
 module.exports = router;
--- routes/view/post.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", function (req, res, next) {
-  res.render("post", { title: "HealthGallery" });
-});
-
-module.exports = router;
