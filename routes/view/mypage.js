097cea8dbc0eefe664b27e01493307962649fb16
20173163 <wjddn494@soongsil.ac.kr>
2020-11-29 02:36:49 +0900

    mypage ejs isLogin mod

--- routes/view/mypage.js
+++ routes/view/mypage.js
@@ -2,17 +2,19 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../models");
 
+// router.get("/", function (req, res, next) {
+//   res.render("mypage", { title: "HealthGallery" });
+// });
 
-router.get("/", function (req, res, next) {
-  res.render("mypage", { title: "HealthGallery" });
-});
-
-router.get("/myinfo", async function (req, res, next) {
+router.get("/", async function (req, res, next) {
+  /* #region 로그인 안되어있으면 */
   if (req.user == undefined) {
     console.log("user undefined");
-    return res.sendStatus(400);
+    return res.render("mypage", { title: "HealthGallery" });
   }
+  /* #endregion */
 
+  /* #region 로그인 되어있으면 */
   let user = req.user.id;
   console.log("user: ", user);
 
@@ -23,11 +25,12 @@ router.get("/myinfo", async function (req, res, next) {
       },
     });
     console.log("info:", info);
-    res.render("mypage.ejs", data=info);
+    res.render("mypage", { title: "HealthGallery", data: info });
   } catch (err) {
     console.log(err);
     return res.sendStatus(500);
   }
+  /* #endregion */
 });
 
 module.exports = router;
